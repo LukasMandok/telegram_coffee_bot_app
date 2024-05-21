abcf25f441d8ee83fe6cd66a1fffa2ef78fb90bc
Lukas Mandok <LukasMandok28@gmail.com>
2024-05-21 23:51:29 +0200

    add a reduced vue template

---
+++ src/components/Template.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  arg1: string
+}>()
+</script>
+
+<template>
+  <div>
+    <template>Test Template</template>
+    This is only a non-functional template component with a prop:
+    {{ arg1 }}
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Template from '../components/Template.vue'
+</script>
+
+<template>
+  <main>
+    <Template arg1="argument 1" />
+  </main>
+</template>
